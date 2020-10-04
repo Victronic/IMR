d9697219bcf33face6618c30faaf6a5a526a16e5
Victor <44725192+Victronic@users.noreply.github.com>
2020-10-04 17:06:14 +0300

    HomeWork

---
+++ New Unity Project (1)/Assets/Script/Attack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject obj1;
+    public GameObject obj2;
+    public Animator anim;
+
+
+    void Update()
+    {
+        float dist = Vector3.Distance(obj1.transform.position, obj2.transform.position);
+        if(dist < 0.3)
+        {
+            anim.SetBool("Attack", true);
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+        }
+        
+    }
+}
