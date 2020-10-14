6c6f577324bb4b94690bf161503e136cac477a66
Victor <44725192+Victronic@users.noreply.github.com>
2020-10-14 14:15:27 +0300

    Lab2

---
+++ Lab2/Assets/Scripts/Draw.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using VRTK;
+public class Draw : MonoBehaviour
+{
+    public VRTK_ControllerEvents controller;
+    public LineRenderer line;
+    public GameObject chalk;
+    bool isUsed = false;
+    void Start()
+    {
+        controller.TriggerPressed += InteractableObjectUsed;
+        controller.TriggerReleased += InteractableObjectUnused;
+    }
+    void Update()
+    {
+        
+        if (isUsed)
+        {
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, chalk.transform.position);
+            
+        }
+    }
+    protected virtual void InteractableObjectUsed (object sender, ControllerInteractionEventArgs e)
+    {
+        isUsed= true;
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, ControllerInteractionEventArgs e)
+    {
+        isUsed = false;
+    }
+}
